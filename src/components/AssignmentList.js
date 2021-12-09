d621e2d09d5b66b479b258b94152f75d22dc9446
Obie <obiemunozjr@gmail.com>
2021-12-09 18:13:19 -0500

    Updated assignmentList

--- src/components/AssignmentList.js
+++ src/components/AssignmentList.js
@@ -8,17 +8,19 @@ const AssignmentList = ({ assignments }) => {
     <div className="assignment-list">
       {assignments.map((assignment) => (
         <Link to={`/details/${assignment.id}`} key={assignment.id}>
-          <h4 className="page-title">{assignment.title}</h4>
-          <p>{assignment.description}</p>
-          <p className="question-count">
-            <em>
-              {assignment?.questions?.length > 0
-                ? assignment?.questions?.length > 1
-                  ? `${assignment?.questions?.length} questions`
-                  : `${assignment?.questions?.length} question`
-                : "No questions"}
-            </em>
-          </p>
+          <h4 className="page-title">
+            {assignment.title}
+            <p id="question-count">
+              <em>
+                {assignment?.questions?.length > 0
+                  ? assignment?.questions?.length > 1
+                    ? `${assignment?.questions?.length} questions`
+                    : `${assignment?.questions?.length} question`
+                  : "No questions"}
+              </em>
+            </p>
+          </h4>
+          <p id="assignment-description">{assignment.description}</p>
         </Link>
       ))}
     </div>
