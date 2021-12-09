a8e886e371b4a644bba42e8c9311612bd528d3c0
Obie <obiemunozjr@gmail.com>
2021-12-08 23:48:19 -0500

    Fixed markdown and added black text to css for markdown

--- src/pages/details/DetailSummary.js
+++ src/pages/details/DetailSummary.js
@@ -1,14 +1,24 @@
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
 
-export default function DetailSummary({questions}) {
-
+export default function DetailSummary({ questions }) {
   return (
     <div>
-      {questions.map((question) =>(
-      <div key={question.id} className="assignment-summary">
-        <h2 className="page-title">{question.question}</h2>
-        <ReactMarkdown className="desc">{question.answer === null ? "this question has not been answered" : question.answer.replace(/["]g/, '')}</ReactMarkdown>
-      </div>))}
+      {questions.map((question) => (
+        <div key={question.id} className="assignment-summary">
+          <h2 className="page-title">{question.question}</h2>
+          {question.answer === null ? (
+            "This question has not been answered"
+          ) : (
+            <ReactMarkdown
+              className="desc"
+              children={question.answer
+                .split("\n")
+                .map((line) => line.trim())
+                .join("\n")}
+            />
+          )}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
