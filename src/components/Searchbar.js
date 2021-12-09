d9fe30639d5a750765900f366dea4e2881e7398e
Obie <obiemunozjr@gmail.com>
2021-12-09 16:26:01 -0500

    Updated searchbar.js - maintains state of current page, searchbar now controlled form

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,30 +1,51 @@
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // styles
-import './Searchbar.css'
+import "./Searchbar.css";
 
 export default function Searchbar() {
-  const [term, setTerm] = useState('')
-  const history = useHistory()
+  const [term, setTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState("/");
+  const history = useHistory();
+
+  history.listen((location) => {
+    if (
+      location.pathname === "/" ||
+      location.pathname === "/phase1" ||
+      location.pathname === "/phase2" ||
+      location.pathname === "/phase3" ||
+      location.pathname === "/phase4"
+    ) {
+      setCurrentPage(location.pathname);
+    } else {
+      return;
+    }
+  });
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-
-    history.push(`/search?q=${term}`)
-  }
+    e.preventDefault();
+    if (term === "") {
+      setTerm("");
+      history.push(currentPage);
+    } else {
+      setTerm("");
+      history.push(`/search?q=${term}`);
+    }
+  };
 
   return (
     <div className="searchbar">
       <form onSubmit={handleSubmit}>
         <label htmlFor="search">Search:</label>
-        <input 
-          id="search" 
+        <input
+          id="search"
           type="text"
-          placeholder="Search Assignments" 
-          onChange={(e) => setTerm(e.target.value)}  
+          placeholder="Search Assignments"
+          onChange={(e) => setTerm(e.target.value)}
+          value={term}
         />
       </form>
     </div>
-  )
+  );
 }
