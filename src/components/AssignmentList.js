d8c50e7b8d6eacee191dc3c31e5c6b96b2d65cf2
Obie <obiemunozjr@gmail.com>
2021-12-09 17:20:20 -0500

    Update Assignments with question count

--- src/components/AssignmentList.js
+++ src/components/AssignmentList.js
@@ -1,19 +1,28 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //styles
-import "./AssignmentList.css"
+import "./AssignmentList.css";
 
-const AssignmentList = ({assignments}) => {
-  return ( 
+const AssignmentList = ({ assignments }) => {
+  return (
     <div className="assignment-list">
       {assignments.map((assignment) => (
-      <Link to={`/details/${assignment.id}`} key={assignment.id}>
-        <h4 className="page-title">{assignment.title}</h4>
-        <p>{assignment.description}</p>
-      </Link>
+        <Link to={`/details/${assignment.id}`} key={assignment.id}>
+          <h4 className="page-title">{assignment.title}</h4>
+          <p>{assignment.description}</p>
+          <p className="question-count">
+            <em>
+              {assignment.questions.length > 0
+                ? assignment.questions.length > 1
+                  ? `${assignment.questions.length} questions`
+                  : `${assignment.questions.length} question`
+                : "No questions"}
+            </em>
+          </p>
+        </Link>
       ))}
     </div>
-   );
-}
- 
+  );
+};
+
 export default AssignmentList;
