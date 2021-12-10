1a7632e25cdeff3137abc2d85e379d37ce4d0820
Obie <obiemunozjr@gmail.com>
2021-12-09 22:41:31 -0500

    Updated DetailSummary if no questions found

--- src/pages/details/DetailSummary.js
+++ src/pages/details/DetailSummary.js
@@ -1,24 +1,41 @@
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
 
 export default function DetailSummary({ questions }) {
+  const questionList = questions.map((question) => {
+    return (
+      <div key={question.id} className="assignment-summary">
+        <h2 className="page-title">{question.question}</h2>
+        {question.answer === null ? (
+          <p>No answer provided</p>
+        ) : (
+          <ReactMarkdown
+            className="desc"
+            children={question.answer
+              .split("\n")
+              .map((line) => line.trim())
+              .join("\n")}
+          />
+        )}
+      </div>
+    );
+  });
+
   return (
     <div>
-      {questions.map((question) => (
-        <div key={question.id} className="assignment-summary">
-          <h2 className="page-title">{question.question}</h2>
-          {question?.answer === null ? (
-            "This question has not been answered"
-          ) : (
-            <ReactMarkdown
-              className="desc"
-              children={question.answer
-                .split("\n")
-                .map((line) => line.trim())
-                .join("\n")}
-            />
-          )}
-        </div>
-      ))}
+      <h1>Assignment Questions</h1>
+      {questionList.length === 0 ? (
+        <>
+          <p>
+            <em>No questions have been added to this assignment</em>
+          </p>
+          <Link to="/create" className="question-btn">
+            Ask a Question!
+          </Link>
+        </>
+      ) : (
+        questionList
+      )}
     </div>
   );
 }
