a50911ba1f4cb68412f7e75d7533b1b864a09cfe
Obie <obiemunozjr@gmail.com>
2021-12-10 17:40:51 -0500

    update for deployment

--- src/components/CourseLinks.js
+++ src/components/CourseLinks.js
@@ -1,16 +1,22 @@
-import { NavLink } from 'react-router-dom'
-import DashboardIcon from '../assets/dashboard_icon.svg'
+import { NavLink } from "react-router-dom";
+import DashboardIcon from "../assets/dashboard_icon.svg";
 
-
-const CourseLinks = ({courses}) => {
-  return ( 
+const CourseLinks = ({ courses }) => {
+  return (
     <li>
-    <NavLink exact to={courses.title === "Prework"? '/' : `/phase${courses.title.slice(-1)}`}>
-      <img src={DashboardIcon} alt="dashboard icon" />
-      <span>{courses.title}</span>
-    </NavLink>
-  </li>
-   );
-}
- 
+      <NavLink
+        exact
+        to={
+          courses.title === "Prework"
+            ? "/fis-assist"
+            : `/phase${courses.title.slice(-1)}`
+        }
+      >
+        <img src={DashboardIcon} alt="dashboard icon" />
+        <span>{courses.title}</span>
+      </NavLink>
+    </li>
+  );
+};
+
 export default CourseLinks;
