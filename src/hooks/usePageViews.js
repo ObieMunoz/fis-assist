a5bda91c05f7447135beab27ba99ed54fcbacca2
Obie <obiemunozjr@gmail.com>
2021-12-10 23:35:56 -0500

    updated analytics

--- src/hooks/usePageViews.js
+++ src/hooks/usePageViews.js
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 function usePageViews() {
     let location = useLocation();
     useEffect(() => {
         if (!window.GA_INITIALIZED) {
-            ReactGA.initialize("G-JJCB5XWQM3", { siteSpeedSampleRate: 100 });
+            ReactGA.initialize("G-JJCB5XWQM3");
             window.GA_INITIALIZED = true;
         }
         ReactGA.set({ page: location.pathname });
-        ReactGA.pageview(location.pathname);
+        ReactGA.send("pageview");
     }, [location]);
 }
 
