1225b2ce749d04d280e15c7e4ac1fcff89bd3491
Obie <obiemunozjr@gmail.com>
2021-12-10 19:20:45 -0500

    update routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,24 @@
-import { Link } from 'react-router-dom'
-import Logo from '../assets/fis_logo.svg'
+import { Link } from "react-router-dom";
+import Logo from "../assets/fis_logo.svg";
 
 // styles & images
-import './Navbar.css'
-import Searchbar from './Searchbar'
+import "./Navbar.css";
+import Searchbar from "./Searchbar";
 
 export default function Navbar() {
-
   return (
     <nav className="navbar">
       <ul>
-          <li className="logo">
-            <img src={Logo} alt="logo" />
+        <li className="logo">
+          <img src={Logo} alt="logo" />
+        </li>
+        <>
+          <Searchbar />
+          <li>
+            <Link to="/fis-assist/about">About Us</Link>
           </li>
-          <>
-            <Searchbar />
-            <li><Link to="/about">About Us</Link></li>
-          </>
+        </>
       </ul>
     </nav>
-  )
+  );
 }
